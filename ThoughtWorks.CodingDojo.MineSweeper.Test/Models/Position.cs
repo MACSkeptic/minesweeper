689b52649b58831da825bb4a5c653b46ca42fad8
macskeptic <macskeptic@gmail.com>
2011-02-19 00:34:20 -0200

    removed everything, added position

---
+++ ThoughtWorks.CodingDojo.MineSweeper.Test/Models/Position.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace ThoughtWorks.CodingDojo.MineSweeper.Test.Models
+{
+    public class Position : IEquatable<Position>
+    {
+        private readonly int _row;
+        private readonly int _column;
+
+        public Position(int row, int column)
+        {
+            _row = row;
+            _column = column;
+        }
+
+        public bool Equals(Position other)
+        {
+            if (ReferenceEquals(null, other))
+            {
+                return false;
+            }
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+            return other._row == _row && other._column == _column;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+            if (obj.GetType() !=
+                typeof(Position))
+            {
+                return false;
+            }
+            return Equals((Position)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (_row * 397) ^ _column;
+            }
+        }
+
+        public static bool operator ==(Position left, Position right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Position left, Position right)
+        {
+            return !Equals(left, right);
+        }
+
+        private int DistanceTo(Position position)
+        {
+            return Math.Max(
+                Math.Abs(position._row - _row), 
+                Math.Abs(position._column - _column));
+        }
+
+        public virtual bool IsNeighbourOf(Position position)
+        {
+            return DistanceTo(position) == 1;
+        }
+    }
+}
--- ThoughtWorks.CodingDojo.MineSweeper.Test/Models/PositionTest.cs
+++ ThoughtWorks.CodingDojo.MineSweeper.Test/Models/PositionTest.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ThoughtWorks.CodingDojo.MineSweeper.Models;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ThoughtWorks.CodingDojo.MineSweeper.Test.Models
 {
@@ -7,66 +10,26 @@ namespace ThoughtWorks.CodingDojo.MineSweeper.Test.Models
     public class PositionTest
     {
         [TestMethod]
-        public void ShouldReturnStringRepresentationContainningRowAndColumn()
+        public void ShouldBeAbleToEvaluateIfItIsNeighbourOfAnotherPosition()
         {
-            var position = new Position(4, 2);
-            Assert.AreEqual("Position@4|2", position.ToString());
-        }
+            var positionA = new Position(1, 1);
+            var positionB = new Position(2, 1);
+            var positionC = new Position(0, 1);
+            var positionD = new Position(1, 2);
+            var positionE = new Position(3, 2);
+            var positionF = new Position(3, 3);
 
-        [TestMethod]
-        public void ShouldConsiderTwoPositionsWithTheSameRowAndColumnToBeEqual()
-        {
-            var position = new Position(4, 2);
-            Assert.IsTrue(position.Equals(new Position(4, 2)));
-            Assert.IsTrue(position.Equals(position));
-            Assert.IsTrue(position == new Position(4, 2));
-            Assert.IsTrue(position == position);
-            Assert.IsFalse(position != new Position(4, 2));
-            Assert.IsFalse(position != position);
-        }
-
-        [TestMethod]
-        public void ShouldConsiderTwoPositionsWithDivergingRowsOrColumnsToBeDifferent()
-        {
-            var position = new Position(4, 2);
-            var differentRow = new Position(3, 2);
-            var differentColumn = new Position(4, 3);
-         
-            Assert.IsFalse(position.Equals(differentRow));
-            Assert.IsFalse(position.Equals(differentColumn));
-            Assert.IsFalse(position == differentRow);
-            Assert.IsFalse(position == differentColumn);
-            Assert.IsTrue(position != differentRow);
-            Assert.IsTrue(position != differentColumn);
-        }
-
-        [TestMethod]
-        public void ShouldConsiderDistanceToItselfToBeZero()
-        {
-            var position = new Position(4, 2);
-            Assert.AreEqual(0, position.DistanceTo(position));
-        }
-
-        [TestMethod]
-        public void ShouldConsiderDistanceToItsNeighboursToBe1()
-        {
-            var position = new Position(4, 2);
-            var positionN = new Position(5, 2);
-            var positionS = new Position(3, 2);
-            var positionE = new Position(4, 3);
-            var positionNE = new Position(5, 3);
-            var positionSE = new Position(3, 3);
-            var positionW = new Position(4, 1);
-            var positionNW = new Position(5, 1);
-            var positionSW = new Position(3, 1);
-            Assert.AreEqual(1, position.DistanceTo(positionN));
-            Assert.AreEqual(1, position.DistanceTo(positionS));
-            Assert.AreEqual(1, position.DistanceTo(positionE));
-            Assert.AreEqual(1, position.DistanceTo(positionW));
-            Assert.AreEqual(1, position.DistanceTo(positionNE));
-            Assert.AreEqual(1, position.DistanceTo(positionSE));
-            Assert.AreEqual(1, position.DistanceTo(positionNW));
-            Assert.AreEqual(1, position.DistanceTo(positionSW));
+            Assert.AreEqual(false, positionA.IsNeighbourOf(positionA));
+            Assert.AreEqual(true, positionA.IsNeighbourOf(positionB));
+            Assert.AreEqual(true, positionB.IsNeighbourOf(positionA));
+            Assert.AreEqual(true, positionA.IsNeighbourOf(positionC));
+            Assert.AreEqual(true, positionC.IsNeighbourOf(positionA));
+            Assert.AreEqual(true, positionA.IsNeighbourOf(positionD));
+            Assert.AreEqual(true, positionD.IsNeighbourOf(positionA));
+            Assert.AreEqual(false, positionA.IsNeighbourOf(positionE));
+            Assert.AreEqual(false, positionE.IsNeighbourOf(positionA));
+            Assert.AreEqual(false, positionA.IsNeighbourOf(positionF));
+            Assert.AreEqual(false, positionF.IsNeighbourOf(positionA));
         }
     }
 }
